96af3429413babb66d6b57190a5cbdf4241f1391
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2024-04-11 22:23:58 -0700

    Implement MP3 tag read

--- fileIO.js
+++ fileIO.js
@@ -1,11 +1,28 @@
 
 
-function dropHandler(input){
-    printFileNamees(input);
+function dropHandler(event){
+    event.preventDefault(); // This needs to be event.preventDefault(), not input.preventDefault()
+    var items = event.dataTransfer.items;
+    if (items) {
+        // Use DataTransferItemList interface to access the files
+        for (var i = 0; i < items.length; i++) {
+            // If dropped items aren't files, reject them
+            if (items[i].kind === 'file') {
+                var file = items[i].getAsFile();
+                readMusicTags(file);
+            }
+        }
+    }
+}
+
+function dragOverHandler(event){
+    event.preventDefault(); // TODO: Add a visual cue to show that the file can be dropped!!!
 }
 
 function handleFiles(files){
-    printFileNames(files);
+    for(var i = 0; i < files.length; i++){
+        readMusicTags(files[i]);
+    }
 }
 
 function printFileNames(inputFolder){
@@ -13,8 +30,18 @@ function printFileNames(inputFolder){
     console.log(inputFolder[0]);
     console.log(typeof inputFolder[0]);
 
+}
 
-    // for(var i = 0; i < inputFolder.length; i++){
-    //     console.log(inputFolder[i].name);
-    // }
+//TODO: Now that you can access the metadata, organize it into a suitable datastructure
+function readMusicTags(file){
+    jsmediatags.read(file, {
+        onSuccess: function(tag){
+            console.log('Title:' + tag.tags.title);
+            console.log('Artist:' + tag.tags.artist);
+            console.log('Album:' + tag.tags.album);
+        },
+        onError: function(error){
+            console.log(error);
+        }
+    });
 }
