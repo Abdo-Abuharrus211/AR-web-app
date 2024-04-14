e183016e18ea3adce185b089f1b32e0d5509ce40
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2024-04-14 15:12:11 -0700

    update HTML to reflect folder drop

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 // Here we'll have scripts that modify the DOM
 // import { folderAdded, metadata } from './fileIO';
 var playlistNameValue = "";
-var folderAdded = false;
+var isFolderAdded = false;
 
 const playlistNameInput = document.getElementsByClassName("playlist-form")[0];
 playlistNameInput.addEventListener("input", () => {
     playlistNameValue = playlistNameInput.value;
-    validateInput();
-}
-);
-
-
-const folderInput = document.getElementById("folderInput");
-folderInput.addEventListener("change", (e) => {
-    folderAdded = Array.from(e.target.files).every(file => file.type === "audio/mpeg");
+    console.log(playlistNameValue);
     validateInput();
 });
 
+
+const folderInput = document.getElementById("folderInput");
+folderInput.addEventListener("change", handleFolderInput);
+folderInput.addEventListener("drop", handleFolderInput);
+function handleFolderInput(e) {
+    if (Array.from(e.target.files).some(file => file.type === "audio/mpeg")){
+        isFolderAdded = true;
+    }
+    console.log(isFolderAdded);
+    validateInput();
+}
+
 function validateInput() {
-    // TODO: constantly check that both a folder (metadata) has been dropped or added
-    //  and that playlistNameValue is not empty string
-    // Then set the Harvest btn to valid
-    if(folderAdded == true && playlistNameValue != ""){
+    if(isFolderAdded == true && playlistNameValue != ""){
         document.getElementById("harvest-btn").disabled = false;
         console.log("ready to harvest");
         }
