652cbd2cf2a6b15893899881db1a423aae748296
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-09-28 14:54:04 -0700

    Refactor the modal handling module and change to TypeScript

--- reaper-reborn/src/scripts/privacyAndTerms.ts
+++ reaper-reborn/src/scripts/privacyAndTerms.ts
@@ -1,46 +1,54 @@
-let currentModal = null;
-// Opening modals
+let currentModal: string = "";
+
+// The Privacy Policy Modal
 const privacyModalTrigger = document.getElementsByClassName("policy-trigger");
 for (let trigger of privacyModalTrigger) {
     currentModal = "privacy-policy-modal";
     trigger.addEventListener("click", () => openModal("privacy-policy-modal"));
 }
+
+document.getElementById("close-privacy-modal")?.addEventListener("click", () => {
+    closeModal("privacy-policy-modal");
+});
+
+// The Terms of Service Modal
 const termsModalTrigger = document.getElementsByClassName("terms-trigger");
 for (let trigger of termsModalTrigger) {
     currentModal = "terms-modal";
     trigger.addEventListener("click", () => openModal("terms-modal"));
 }
 
-
-const privacyModalCloser = document.getElementById("close-privacy-modal").addEventListener("click", function() {
-    closeModal("privacy-policy-modal");
-});
-const termsModalCloser = document.getElementById("close-terms-modal").addEventListener("click", function() {
+document.getElementById("close-terms-modal")?.addEventListener("click", () => {
     closeModal("terms-modal");
 });
 
 // Close modal on "Esc" key press
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", (event) => {
     if (event.key === "Escape" && currentModal) {
         closeModal(currentModal);
     }
 });
 
+
 // Assuming currentModal is correctly set when opening a modal
-document.addEventListener("click", function (event) {
+document.addEventListener("click", (event) => {
     if (currentModal && event.target === document.getElementById(currentModal)) {
         closeModal(currentModal);
     }
 });
 
-function openModal(modalName) {
-    var modal = document.getElementById(modalName);
-    modal.classList.remove("hidden");
-    currentModal = modalName;
+function openModal(modalName: string) {
+    const modal = document.getElementById(modalName);
+    if (modal) {
+        modal.classList.remove("hidden");
+        currentModal = modalName;
+    }
 }
 
-function closeModal(modalName) {
-    var modal = document.getElementById(modalName);
-    modal.classList.add("hidden");
-    currentModal = null;
+function closeModal(modalName: string) {
+    const modal = document.getElementById(modalName);
+    if (modal) {
+        modal.classList.add("hidden");
+        currentModal = "";
+    }
 }
